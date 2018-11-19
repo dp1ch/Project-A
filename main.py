623cd0fd2bc69bcb93cdeb9a8a9c384118cee11a
myloginmy <44487037+myloginmy@users.noreply.github.com>
2018-11-19 23:54:26 +0700

    New Project with new idea - Hangman Game

--- main.py
+++ main.py
@@ -1,136 +1,89 @@
-#The TurtleBalls By Denis Pichuev
-#Project
-class Point:
-	def __init__(self,x,y):
-		self.x=x
-		self.y=y
-class Vector:	
-	def __init__(self,x,y):
-		self.x=x
-		self.y=y
-	def getX(self):
-		return self.x
-	def getY(self):
-		return self.y	
-class Ball:	
-	def __init__(self,position,velocity,color,mass):
-		self.velocity=velocity
-		self.position=position
-		self.color=color
-		self.mass=mass
-	def move(self):
-		Oldpositionx=self.position.x
-		Oldpositiony=self.position.y
-		self.position.x+=self.velocity.x
-		self.position.y+=self.velocity.y
-		turtle.pu()
-		turtle.goto(self.position.x,self.position.y)
-		turtle.pd()
-		turtle.pencolor(self.color)
-		turtle.goto(Oldpositionx,Oldpositiony)
-		turtle.pu()
-		return self
-	def draw(self,bcolor):
-		turtle.pu()
-		Lcolor=turtle.pencolor()
-		
-		turtle.pencolor("black")
-		turtle.goto(self.position.x,self.position.y-25)
-		
-		turtle.pencolor(Lcolor)
-		turtle.fillcolor(bcolor)
-		turtle.begin_fill()
-		turtle.pd()
-		turtle.circle(25)
-		turtle.pu()
-		turtle.end_fill()
-	def checkBorder(self):
-		
-		turtle.pencolor("black")
-		windowX=500
-		windowY=500
-		windowY0=-500
-		windowX0=-500
-		
-		if self.position.x+25+self.velocity.x>windowX:
-			self.velocity.x*=-1
-		if self.position.x-25+self.velocity.x<windowX0:
-			self.velocity.x*=-1
-		if self.position.y+50+self.velocity.y>windowY:
-			self.velocity.y*=-1
-		if self.position.y-25+self.velocity.y<windowY0:
-			self.velocity.y*=-1
-		return self
-def CreateList():
-	list= []
-	
-	turtle.pencolor("black")
-	for i in range(1):
-		list.append(Ball(Point(i*100+100,100),Vector(50,-10),"white",700))
-		list.append(Ball(Point(300,200),Vector(-40,-10),"brown",500))
-		list.append(Ball(Point(200,200),Vector(40,10),"red",80))
-		list.append(Ball(Point(200,300),Vector(-60,+70),"blue",50))
-		list.append(Ball(Point(200,150),Vector(-90,+50),"green",100))
-		
-	return list
+import random
+import local
+def RefreshWord(picked,word,secret):
+    
+    for i in range(len(word)):
+        result=""
+        F=0
+        if secret[i]==picked:
+            word[i]=picked
+            F+=1
+    if F>1:
+        print(local.UFIND,F,local.LETTERS)
+    for i in word:
+    	result+=str(i)
+    print(result)
+    print(tries)
+    return word
+    
 
-def MoveBalls(BallList):
-	
-		turtle.pencolor("black")
-		for i in BallList:
-			i=i.checkBorder()
-		for i in BallList:
-			i=i.move()
-		return BallList
-def DrawBalls(BallList):
-		turtle.pencolor("black")
-		turtle.pd()
-		for i in BallList:
-			turtle.pencolor(i.color)
-			i=i.draw(i.color)
-		
-		turtle.pencolor("black")
-def ClearBalls(BallList):
-		turtle.pencolor("black")
-		turtle.pd()
-		for i in BallList:
-			i=i.draw("black")
-		for i in BallList:
-			i=i.draw("black")
-		turtle.pu()		
-import turtle
-turtle.begin_fill()
-turtle.pencolor("black")
-turtle.pu()
-turtle.ht()
-turtle.speed(0)
-windowX=500
-windowY=500
-windowY0=-500
-windowX0=-500
-turtle.setpos(windowX,windowY0)
-turtle.pd()
-turtle.setpos(windowX,windowY)
-turtle.goto(windowX0,windowY)
-turtle.goto(windowX0,windowY0)
-turtle.goto(windowX,windowY0)
-turtle.pd()
-turtle.fillcolor("black")
-turtle.end_fill()
-BallList=CreateList()
-while True:
-	turtle.pencolor("black")
-	turtle.pu()
-	BallList=MoveBalls(BallList)
-	turtle.pu()	
-	DrawBalls(BallList)
-	ClearBalls(BallList)
-
-
-#Frame
-
-#Text
-
-#Picture
-
-#Mark
+Sport=local.SPORT
+Transport=local.TRANSPORT
+Biology=local.BIOLOGY
+Politics=local.POLITICS
+Geography=local.GEOGRAPHY
+def Generate():
+    a=input(local.CHOOSE).lower()
+    category="Политика"
+    if a =="с":
+        category="Спорт"
+        pull=Sport
+    elif a == "т":
+        category="Транспорт"
+        pull=Transport
+    elif a=="б":
+        category="Биология"
+        pull=Biology
+    elif a=="п":
+        category="Политика"
+        pull=Politics
+    elif a=="г":
+        category="География"
+        
+        pull=Geography
+    else:
+        category="Биология"
+        pull=Biology
+    secret=pull[random.randint(0,len(pull)-1)].lower()
+    return secret,pull,category
+secret,pull,category=Generate()
+tries=5
+word=[]
+ban={',','*','&','.','!','1','2','3','4','5','6','7','8','9','?','%',':',';','$','#','№'}
+flag=True
+print(local.START1,tries,local.START2,category,local.SIGN)
+for i in (secret):
+    word.append("*")
+word=RefreshWord(' ',word,secret)
+while tries!=0:
+    if '*' not in word:
+        print(local.WON)
+        if input(local.RESTART) =="р":
+            secret,pull,category=Generate()
+            tries=5
+            word=[]
+            for i in (secret):
+                word.append("*")
+            flag=True
+            continue
+    if flag==True:
+	    flag=False
+	    result=""
+	    for i2 in word:
+	    	  result+=str(i2)
+	    print(result)
+    picked=input(local.CHOOSELETTER).lower()
+    
+    if len(picked)>1 or picked in ban:
+        print(local.PROHIBITED)
+        
+    if picked in secret:
+        print(local.SCORED)
+        word=RefreshWord(picked,word,secret)
+        continue
+    print(local.NOTSCORED)
+    tries-=1
+    print(tries)
+if tries==0:
+    print(local.FINAL,secret)
+    
